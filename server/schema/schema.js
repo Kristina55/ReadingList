e49ea29c163120d70abc08460dab14f55458bc4b
kristina <kristinaa.stojanovska@gmail.com>
2020-04-14 20:18:56 -0700

    created fake db for now, installed lodash

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,15 @@
 const graphql = require("graphql");
+const _ = require("lodash");
 
 const { GraphQLObjectType, GraphQLString } = graphql;
+
+// I have installed lodash to find data or change data easily
+
+let books = [
+  { name: "Name of the Wind", genre: "Fantazy", id: "1" },
+  { name: "The Final Empire", genre: "Fantazy", id: "2" },
+  { name: "The Long Earth", genre: "Sci-Fi", id: "3" },
+];
 
 const BookType = GraphQLObjectType({
   name: Book,
@@ -17,7 +26,11 @@ const RootQuery = new GraphQLObjectType({
     book: {
       type: BookType,
       args: { id: { type: GraphQLString } },
-      resolve(parent, ards) {},
+      resolve(parent, ards) {
+        // using lodash loop trough the array and find the book with the id
+        // that is attached to the args property
+        return _.find(books, { id: args.id });
+      },
     },
   },
 });
