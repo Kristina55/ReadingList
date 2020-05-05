f135b5085ee83b077cea820709fbeaea2130a7c8
kristina <kristinaa.stojanovska@gmail.com>
2020-05-05 16:42:08 -0700

    Mongoose Models and Mutations

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,7 @@
 const graphql = require("graphql");
 const _ = require("lodash");
+const Book = require("../models/book");
+const Author = require("../models/author");
 
 const {
   GraphQLObjectType,
@@ -11,20 +13,6 @@ const {
 } = graphql;
 
 // I have installed lodash to find data or change data easily
-let books = [
-  { name: "Name of the Wind", genre: "Fantazy", id: "1", authorId: "1" },
-  { name: "The Final Empire", genre: "Fantazy", id: "2", authorId: "2" },
-  { name: "The Long Earth", genre: "Sci-Fi", id: "3", authorId: "3" },
-  { name: "The Hero of Ages", genre: "Fantasy", id: "4", authorId: "2" },
-  { name: "The Color of Magic", genre: "Fantasy", id: "5", authorId: "3" },
-  { name: "The Light Fantastic", genre: "Fantasy", id: "6", authorId: "3" },
-];
-
-let authors = [
-  { name: "Patrick Rothfuss", age: 44, id: "1" },
-  { name: "Brandon Sanderson", age: 42, id: "2" },
-  { name: "Terry Pratchett", age: 63, id: "3" },
-];
 
 // Type Relation that I am looking for
 // {
@@ -50,7 +38,7 @@ const BookType = new GraphQLObjectType({
       // (name, genre, id, authorId)
       resolve(parent, args) {
         console.log(parent);
-        return _.find(authors, { id: parent.authorId });
+        // return _.find(authors, { id: parent.authorId });
       },
     },
   }),
@@ -65,7 +53,7 @@ const AuthorType = new GraphQLObjectType({
     books: {
       type: new GraphQLList(BookType),
       resolve(parent, args) {
-        return _.filter(books, { authorId: parent.id });
+        //  return _.filter(books, { authorId: parent.id });
       },
     },
   }),
@@ -80,26 +68,49 @@ const RootQuery = new GraphQLObjectType({
       resolve(parent, args) {
         // using lodash loop trough the array and find the book with the id
         // that is attached to the args property
-        return _.find(books, { id: args.id });
+        // return _.find(books, { id: args.id });
       },
     },
     author: {
       type: AuthorType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return _.find(authors, { id: args.id });
+        // return _.find(authors, { id: args.id });
       },
     },
     books: {
       type: new GraphQLList(BookType),
       resolve(parent, args) {
-        return books;
+        // return books;
       },
     },
     authors: {
       type: new GraphQLList(AuthorType),
       resolve(parent, args) {
-        return authors;
+        // return authors;
+      },
+    },
+  },
+});
+
+const Mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    // if someone adds new author from the UI
+    addAuthor: {
+      type: AuthorType,
+      args: {
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt },
+      },
+      resolve(parent, args) {
+        // import the Author model from models folder
+        let author = new Author({
+          name: args.name,
+          age: args.age,
+        });
+        //save to database (power of mongoose)
+        return author.save();
       },
     },
   },
@@ -107,4 +118,5 @@ const RootQuery = new GraphQLObjectType({
 
 module.exports = new GraphQLSchema({
   query: RootQuery,
+  mutation: Mutation,
 });
