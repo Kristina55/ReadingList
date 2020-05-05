c3ff4d2da231c5e7eba452e12ed8876a8ba94140
kristina <kristinaa.stojanovska@gmail.com>
2020-05-05 13:45:50 -0700

    connecting to MongoDB

--- server/app.js
+++ server/app.js
@@ -2,8 +2,18 @@ const express = require("express");
 // provides to express understand graphQL
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
-
+const mongoose = require("mongoose");
 const app = express();
+
+const uri =
+  "mongodb+srv://Kristina:test123@cluster0-gickw.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function (err, resp) {
+  console.log("********** connected to the database");
+});
 
 app.use("/graphql", graphqlHTTP({ schema: schema, graphiql: true }));
 
