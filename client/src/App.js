68a44f2fbe43cb4c1c0217cc4c26af35d7de4eee
kristina <kristinaa.stojanovska@gmail.com>
2020-06-08 15:02:45 -0700

    added a comment

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
-import React from "react";
-import ApolloClient from "apollo-boost";
+import React from 'react';
+import ApolloClient from 'apollo-boost';
 // to bind apollo to react using "react-apollo" package
-import { ApolloProvider } from "react-apollo";
+import { ApolloProvider } from 'react-apollo';
 
 //components
-import BookList from "./components/BookList";
-import AddBook from "./components/AddBook";
+import BookList from './components/BookList';
+import AddBook from './components/AddBook';
 
 //apollo client setup
 // prettier-ignore
@@ -13,20 +13,21 @@ const client = new ApolloClient({
   uri: "http://localhost:4000/graphql"
 });
 
+// Reading List app
 class App extends React.PureComponent {
-  render() {
-    return (
-      // surounding the entire app and enables to get the data from the GraphQL endpoint
-      // and inject into React app
-      <ApolloProvider client={client}>
-        <div id="main">
-          <h1>Reading List</h1>
-          <BookList />
-          <AddBook />
-        </div>
-      </ApolloProvider>
-    );
-  }
+	render() {
+		return (
+			// surounding the entire app and enables to get the data from the GraphQL endpoint
+			// and inject into React app
+			<ApolloProvider client={client}>
+				<div id="main">
+					<h1>Reading List</h1>
+					<BookList />
+					<AddBook />
+				</div>
+			</ApolloProvider>
+		);
+	}
 }
 
 export default App;
