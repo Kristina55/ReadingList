90fefe435a5ad671fbd2a83acbe272fb742d6dd1
kristina <kristinaa.stojanovska@gmail.com>
2020-06-08 15:20:52 -0700

    adding a comment

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,92 +1,79 @@
-import React from "react";
+import React from 'react';
 // to bind apollo to react using "react-apollo" package
-import { graphql } from "react-apollo";
-import { flowRight as compose } from "lodash";
-import {
-  getAuthorsQuery,
-  addBookMutation,
-  getBooksQuery,
-} from "../queries/queries";
+import { graphql } from 'react-apollo';
+import { flowRight as compose } from 'lodash';
+import { getAuthorsQuery, addBookMutation, getBooksQuery } from '../queries/queries';
 
+// AddBook Class component
 class AddBook extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      genre: "",
-      authorId: "",
-    };
-  }
-  displayAuthors() {
-    let data = this.props.getAuthorsQuery;
-    if (data.loading) {
-      return <option>Loading authors....</option>;
-    } else {
-      return data.authors.map((author) => {
-        return (
-          <option key={author.id} value={author.id}>
-            {author.name}
-          </option>
-        );
-      });
-    }
-  }
-  handleChange = (event) => {
-    this.setState({
-      ...this.state,
-      [event.target.name]: event.target.value,
-    });
-  };
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: '',
+			genre: '',
+			authorId: ''
+		};
+	}
+	displayAuthors() {
+		let data = this.props.getAuthorsQuery;
+		if (data.loading) {
+			return <option>Loading authors....</option>;
+		} else {
+			return data.authors.map((author) => {
+				return (
+					<option key={author.id} value={author.id}>
+						{author.name}
+					</option>
+				);
+			});
+		}
+	}
+	handleChange = (event) => {
+		this.setState({
+			...this.state,
+			[event.target.name]: event.target.value
+		});
+	};
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.props.addBookMutation({
-      variables: {
-        name: this.state.name,
-        genre: this.state.genre,
-        authorId: this.state.authorId,
-      },
-      // to update the list of books immediately, not on refresh
-      refetchQueries: [{ query: getBooksQuery }],
-    });
-  };
-  render() {
-    const { name, genre } = this.state;
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <div className="field">
-          <label>Book Name:</label>
-          <input
-            type="text"
-            onChange={this.handleChange}
-            name="name"
-            value={name}
-          />
-        </div>
-        <div className="field">
-          <label>Genre:</label>
-          <input
-            type="text"
-            onChange={this.handleChange}
-            name="genre"
-            value={genre}
-          />
-        </div>
-        <div className="field">
-          <label>Author:</label>
-          <select onChange={this.handleChange} name="authorId">
-            <option>Select Author:</option>
-            {this.displayAuthors()}
-          </select>
-        </div>
-        <button>+</button>
-      </form>
-    );
-  }
+	handleSubmit = (e) => {
+		e.preventDefault();
+		this.props.addBookMutation({
+			variables: {
+				name: this.state.name,
+				genre: this.state.genre,
+				authorId: this.state.authorId
+			},
+			// to update the list of books immediately, not on refresh
+			refetchQueries: [ { query: getBooksQuery } ]
+		});
+	};
+	render() {
+		const { name, genre } = this.state;
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<div className="field">
+					<label>Book Name:</label>
+					<input type="text" onChange={this.handleChange} name="name" value={name} />
+				</div>
+				<div className="field">
+					<label>Genre:</label>
+					<input type="text" onChange={this.handleChange} name="genre" value={genre} />
+				</div>
+				<div className="field">
+					<label>Author:</label>
+					<select onChange={this.handleChange} name="authorId">
+						<option>Select Author:</option>
+						{this.displayAuthors()}
+					</select>
+				</div>
+				<button>+</button>
+			</form>
+		);
+	}
 }
 // bind the query to the component and have access in props
 // of the data that comes back from the query
 export default compose(
-  graphql(getAuthorsQuery, { name: "getAuthorsQuery" }),
-  graphql(addBookMutation, { name: "addBookMutation" })
+	graphql(getAuthorsQuery, { name: 'getAuthorsQuery' }),
+	graphql(addBookMutation, { name: 'addBookMutation' })
 )(AddBook);
